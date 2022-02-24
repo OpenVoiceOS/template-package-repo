2033a285f32e741ef7c57a65881e11aa162c3ffd
jarbasai <jarbasai@mailfence.com>
2022-02-24 21:17:41 +0000

    :tada:

---
+++ readme.md
@@ -0,0 +1,11 @@
+# replace_package_name
+
+- search and replace the string `replace_package_name` everywhere in this repo
+- create dev and master branches
+- make dev default branch
+- set PYPI_TOKEN in action secrets
+- edit license_tests.py settings to your liking
+- on PR to dev -> alpha version published and github prerelease created
+- merge dev -> master when ready for release
+- manually trigger one of the workflows for major/minor/build release in the actions tab
+- never touch a version file, make a github release or publish to pypi manually again!
---
+++ setup.py
@@ -0,0 +1,64 @@
+import os
+from setuptools import setup
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'replace_package_name', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
+
+
+setup(
+    name='replace_package_name',
+    version=get_version(),
+    packages=['replace_package_name',
+              'replace_package_name.utils'],
+    url='https://github.com/OpenVoiceOS/replace_package_name',
+    license='Apache-2.0',
+    author='jarbasAi',
+    install_requires=required("requirements/requirements.txt"),
+    package_data={'': package_files('replace_package_name')},
+    author_email='jarbasai@mailfence.com',
+    description='OpenVoiceOS package'
+)
